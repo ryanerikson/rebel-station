6c47c3c4fae2505af5b5089477b5797190746183
echel0n <echel0n@terrarebels.net>
2023-01-24 23:54:03 -0800

    Disabled Tax Rewards chart from dashboard

--- src/pages/dashboard/Charts.tsx
+++ src/pages/dashboard/Charts.tsx
@@ -1,23 +1,23 @@
 import classNames from "classnames/bind"
-import { useIsClassic } from "data/query"
+// import { useIsClassic } from "data/query"
 import { useIsTerraAPIAvailable } from "data/Terra/TerraAPI"
 import TxVolume from "../charts/TxVolume"
-import StakingReturn from "../charts/StakingReturn"
-import TaxRewards from "../charts/TaxRewards"
+// import StakingReturn from "../charts/StakingReturn"
+// import TaxRewards from "../charts/TaxRewards"
 import Wallets from "../charts/Wallets"
 import styles from "./Charts.module.scss"
 
 const cx = classNames.bind(styles)
 
 const Charts = () => {
-  const isClassic = useIsClassic()
+  // const isClassic = useIsClassic()
   const available = useIsTerraAPIAvailable()
   if (!available) return null
 
   return (
-    <div className={cx(styles.charts, { trisect: true })}>
+    <div className={cx(styles.charts, { trisect: false })}>
       <TxVolume />
-      {isClassic ? <TaxRewards /> : <StakingReturn />}
+      {/*{isClassic ? <TaxRewards /> : <StakingReturn />}*/}
       <Wallets />
     </div>
   )
